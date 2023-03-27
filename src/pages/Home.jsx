54a6f118d006cab24185bac859e02ae8650d860a
ZNP8b <znp8b00ff@gmail.com>
2023-03-28 00:23:01 +0300

    Initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+
+import Categories from '../components/Categories';
+import Sort from '../components/Sort';
+import PizzaBlock from '../components/PizzaBlock';
+import { Skeleton } from '../components/PizzaBlock/Skeleton';
+import Pagination from '../components/Pagination';
+import { SearchContext } from '../App';
+
+function Home() {
+  const { searchValue } = React.useContext(SearchContext);
+
+  const [items, setItems] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
+  const [categoryId, setCategoryId] = React.useState(0);
+  const [currentPage, setCurrentPage] = React.useState(1);
+  const [sortType, setSortType] = React.useState({
+    name: 'популярности',
+    sortProperty: 'rating',
+  });
+
+  const order = sortType.sortProperty.includes('-') ? 'asc' : 'desc';
+  const sortBy = sortType.sortProperty.replace('-', '');
+  const category = categoryId > 0 ? `category=${categoryId}` : '';
+  const search = searchValue ? `search=${searchValue}` : '';
+
+  React.useEffect(() => {
+    setIsLoading(true);
+    fetch(
+      `https://63e3b959c919fe386c0d6582.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}&${search}`,
+    )
+      .then((res) => res.json())
+      .then((json) => {
+        setItems(json);
+        setIsLoading(false);
+      });
+    window.scrollTo(0, 0);
+  }, [categoryId, sortType, searchValue, currentPage]);
+
+  const pizzas = items.map((pizza) => (
+    <PizzaBlock
+      key={pizza.id}
+      {...pizza}
+    />
+  ));
+
+  const skeletons = [...new Array(6)].map((_, index) => <Skeleton key={index} />);
+
+  return (
+    <div className='container'>
+      <div className='content__top'>
+        <Categories
+          value={categoryId}
+          onChangeCategory={(i) => setCategoryId(i)}
+        />
+        <Sort
+          value={sortType}
+          onChangeSort={(i) => setSortType(i)}
+        />
+      </div>
+      <h2 className='content__title'>Все пиццы</h2>
+      <div className='content__items'>{isLoading ? skeletons : pizzas}</div>
+      <Pagination onChangePage={(number) => setCurrentPage(number)} />
+    </div>
+  );
+}
+
+export default Home;
